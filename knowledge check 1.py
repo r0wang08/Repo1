975fe64851e48fc3bce352c0fe942f0f7a832235
RongQin Wang <rongqinwang35@gmail.com>
2023-02-19 15:48:49 -0500

    knowledge check messsage for github

--- knowledge check 1.py
+++ knowledge check 1.py
@@ -6,3 +6,5 @@ print(the_dict['Location'])
 
 the_tuple = (1, 2.0, 'three', (4, 5, 6.0))
 print(the_tuple[2])
+
+# I am not sure if this is what the instructions wanted me to do. Please give feedback. Thanks.
